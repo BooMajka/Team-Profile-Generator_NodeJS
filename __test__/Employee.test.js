aa673f624ebe8372f57b9c27997682643233b567
BooMajka <maya@Marias-iMac.lan1>
2022-01-12 20:01:54 -0600

    test changes

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -1,22 +1,22 @@
 const Employee = require("../lib/Employee.js")
 
 describe('getName', () => {
-test("returns Employee's name", ()=>{
-    const result = new Employee().getName("Maya");
-    expect(result).toBe("Maya");
+test("returns Employee's name", () => {
+    const result = new Employee("Maya", "1", "maya@gmail.com");
+    expect(result.getName()).toBe("Maya");
 });
 });
 
 describe("getId", () => {
   test("returns Employee's id", () => {
-    const result = new Employee().getId("1");
-    expect(result).toBe("1");
+    const result = new Employee("Maya", "1", "maya@gmail.com");
+    expect(result.getId()).toBe("1");
   });
 });
 
 describe("getEmail", () => {
   test("returns Employee's email", () => {
-    const result = new Employee().getEmail("maya@gmail.com");
-    expect(result).toBe("maya@gmail.com");
+    const result = new Employee("Maya", "1", "maya@gmail.com");
+    expect(result.getEmail()).toBe("maya@gmail.com");
   });
 });
